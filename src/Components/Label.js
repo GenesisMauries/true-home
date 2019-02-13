c3a6896a12039f1349a07bdd86d46bf3620d1a17
GenesisMauries <exodo_54@hotmail.com>
2019-02-13 16:37:47 -0600

    Sube componente Label, contiene titulo

---
+++ src/Components/Label.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Row, Col, Jumbotron} from 'reactstrap';
+
+export default()=>{
+    return(
+        <div>
+        <Row>
+            <Col xs='12'>
+                <Jumbotron className='text-center'>
+                    <h1 className='display-5'>INMUEBLES RECIENTES</h1>
+                    <p className='lead'>Nosotros logramos que comprar sea un proceso ágil y profesional. </p>
+                </Jumbotron>
+            </Col>
+        </Row>
+
+        </div>
+    )
+}
